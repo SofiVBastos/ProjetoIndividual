e41d760ec2e37fe8038548d7e36cc74a3e5a2ddd
SofiVBastos <sofia.bastos@sptech.school>
2022-11-23 13:23:27 -0300

    PI-35 quiz finalizado

--- scripit.js
+++ scripit.js
@@ -1,34 +1,47 @@
 var pontos = 0;
 
-function start(){
+function start() {
   inicio.style.display = "none"
   pergunta1.style.display = "block"
 
 }
 
-function vericar_resposta(pergunta, resposta){
-  var pergunta_atual = document.getElementById(`pergunta${pergunta+1}`)
-  var proxPergunta = document.getElementById(`pergunta${pergunta+2}`)
+function vericar_resposta(pergunta, resposta) {
+  var pergunta_atual = document.getElementById(`pergunta${pergunta + 1}`)
+  var proxPergunta = document.getElementById(`pergunta${pergunta + 2}`)
 
-  for(let i = 0; i < quizArray.length; i++){
-    if(quizArray[i].id == pergunta){
-      if(resposta == quizArray[i].correta){
+  for (let i = 0; i < quizArray.length; i++) {
+    if (quizArray[i].id == pergunta) {
+      if (resposta == quizArray[i].correta) {
         pontos++
         alert("Acertou")
 
-      }else{
+      } else {
         alert("Errou!")
       }
 
+      if (pergunta == 9) {
+        pergunta_atual.style.display = "none"
+        pont.style.display = "block"
+
+      }
       pergunta_atual.style.display = "none"
       proxPergunta.style.display = "block"
-
     }
-
   }
 }
 
 
+function prox() {
+pont.style.display = "none"
+grafic.style.display = "block"
+}
+
+
+function next(){
+  grafic.style.display = "none"
+  coments.style.display = "block"
+}
 
 const ctx = document.getElementById('myChart');
 
@@ -36,10 +49,10 @@ const ctx = document.getElementById('myChart');
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['1', '2', '3', '4', '5', '6'],
+    labels: ['1', '2', '3', '4', '5',],
     datasets: [{
       label: 'Pontuação',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5, 2],
       backgroundColor: [
         'rgba(144, 150, 255, 0.5)',
         'rgba(144, 150, 255, 0.5)',
